0ddcd5aa263051d6ab89642bac499dc2368af39f
timholy <tim.holy@gmail.com>
2014-07-22 06:48:03 -0500

    Add base64 test

--- test/tests.jl
+++ test/tests.jl
@@ -2,6 +2,9 @@ using Codecs
 using Base.Test
 
 n = [1,4,7,9,11,17,243,5247]
+
+@test reinterpret(Int, decode(Base64, encode(Base64, reinterpret(Uint8, n)))) == n
+
 for i in n
     @test decode(BCD, encode(BCD, i)) == i
     @test decode(BCD, encode(BCD, i, true), true) == i
