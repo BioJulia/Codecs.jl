62a9a37d67e465232387b857ff9803e8460a2a7e
Spencer Russell <spencer.f.russell@gmail.com>
2015-09-22 00:19:55 -0400

    fixes 0.4 deprecation warnings

--- test/tests.jl
+++ test/tests.jl
@@ -1,7 +1,7 @@
 
 n = [1,4,7,9,11,17,243,5247]
 
-@test reinterpret(Int, decode(Base64, encode(Base64, reinterpret(Uint8, n)))) == n
+@test reinterpret(Int, decode(Base64, encode(Base64, reinterpret(UInt8, n)))) == n
 
 for i in n
     @test decode(BCD, encode(BCD, i)) == i
